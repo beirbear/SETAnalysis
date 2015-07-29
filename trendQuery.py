ea5501aae3f58fd294907426cd8335eef66540e7
Beir Bear <beir.bear@gmail.com>
2015-07-29 19:22:47 +0700

    trendQuery is used for find the moving pattern of stock price in a specific frame

---
+++ trendQuery.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import os
+import glob
+import sys
+import json
+import re
+import numpy as np
+
+INPUT_FOLDER = '/home/beir/Desktop/stockQueries/dataPool/'
+INT_PARAM = 'CHANGE_VALUE'
+WIN_SIZE = 25
+
+def warning(objs):
+    sys.stderr.write("WARNING: " + objs + "\n")
+
+def sorted_ls(path):
+    mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
+    return list(sorted(os.listdir(path), key=mtime, reverse=True))
+    
+def isFloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+if __name__ == "__main__":
+    # get subfolders
+    if len(sys.argv) != 3:
+        warning("Parameter stock name and win size are required!")
+        exit()
+
+    subFolders = sorted_ls(INPUT_FOLDER)
+    keyWord = sys.argv[1].upper()
+    print("Analyzing " + keyWord + " through " + str(len(subFolders)) + " objects.")
+    
+    WIN_SIZE = int(sys.argv[2])
+    
+    totalList = []
+    
+    # Loop to read throught every object
+    for intCounter in range(0, len(subFolders), WIN_SIZE):
+        # Loop to get the interseting parameter in a certain window size
+        valueList = []
+        for eachDay in range(WIN_SIZE):
+            # Check to make sure that reading data is in range 
+            if intCounter + eachDay < len(subFolders):
+                # Create a target file name
+                targetFile = glob.glob(INPUT_FOLDER + subFolders[intCounter + eachDay] + '/stock_data/*' + keyWord + '*')
+               	
+                # Check that object is exist!
+                if len(targetFile) > 1 or len(targetFile) == 0:
+                    warning("Object not found on " + subFolders[intCounter + eachDay])
+                    continue
+     
+
+                payload = [line.rstrip('\n') for line in open(targetFile[0])]
+                stockObject = json.loads(payload[0])
+
+                # Check for object again                
+                if stockObject['SYMBOL'] == keyWord:
+                    # Read object
+                    # print(str(stockObject) + "\n")
+                    if isFloat(stockObject[INT_PARAM]) == False:
+                        warning("Number of object " + subFolders[intCounter + eachDay] + " is not a number (" + stockObject[INT_PARAM] + ")")
+                        valueList.append(0.0)
+                        continue
+                        
+                    valueList.append(float(stockObject[INT_PARAM]))
+                else:
+                    warning("Object name and content mismated!")
+                    exit(-1)
+        
+        # Calculate region
+        totalList.append(np.mean(valueList))
+
+    # Final Score
+    print("Score: " + str(np.mean(totalList)))
